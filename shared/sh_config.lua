0af305b114a0c76e35aefd9caa8175edac7ec732
Nazu <luxeno103@gmail.com>
2026-09-03 22:15:39 +0900

    refactor: resource name

--- shared/sh_config.lua
+++ shared/sh_config.lua
@@ -139,7 +139,7 @@ config.dispatch = getConfigValue({
 ---]]
 ---@type REC_Library.Shared.Enums.Notify
 config.notify = getConfigValue({
-    { resource = "REC_Notify", value = "rec" },
+    { resource = "REC_Hud", value = "rec" },
     { resource = "okokNotify", value = "okok" },
     { resource = "ox_lib", value = "ox" },
 })
